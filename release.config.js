151a2028e3eac749d0e75c23cf521488eb647fbc
Jakub Freisler <jakub@frsource.org>
2022-09-23 23:05:07 +0200

    feat: initial commit
    Signed-off-by: Jakub Freisler <jakub@frsource.org>

---
+++ release.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "branches": ["main"],
+    "plugins": [
+        ["@semantic-release/commit-analyzer", {
+            "releaseRules": [
+                {"type": "chore", "scope": "deps", "release": "patch"}
+            ]
+        }],
+        ["@semantic-release/npm", {
+            "tarballDir": "release"
+        }],
+        "@semantic-release/git"
+    ],
+    "preset": "angular"
+};
